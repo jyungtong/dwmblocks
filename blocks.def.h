849f4dc4bfffcfe0ffadaa8528e4491a5f215709
jyungtong <huntthetux@gmail.com>
2024-06-30 23:02:51 +0800

    use thermal6 for dell laptop

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{" ", "~/get_volume.sh",	0,		10},
-	{" ", "echo $(($(cat /sys/class/thermal/thermal_zone6/temp) / 1000))\"°C\"",	5,		0},
-	{"", "top -bn1 | grep \"Cpu(s)\" | sed \"s/.*, *\\([0-9.]*\\)%* id.*/\\1/\" | awk '{printf \" %5.1f%%\", 100 - $1}'",	3,		0},
+	{"🎧", "~/get_volume.sh",	0,		10},
+	{"🌡️", "echo $(($(cat /sys/class/thermal/thermal_zone6/temp) / 1000))\"°C\"",	5,		0},
+	{"", "top -bn1 | grep \"Cpu(s)\" | sed \"s/.*, *\\([0-9.]*\\)%* id.*/\\1/\" | awk '{printf \"🖥️%5.1f%%\", 100 - $1}'",	3,		0},
 	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	5,		0},
-	{" ", "echo $(cat /sys/class/power_supply/BAT0/capacity)\"%\"",	5,		0},
+	{"🔋", "echo $(cat /sys/class/power_supply/BAT0/capacity)\"%\"",	5,		0},
 	{"", "date '+%b %d (%a) %T'",					1,		0},
 };
 
