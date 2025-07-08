36ed2bdde102131a12a3e6ba393c426ccb751adf
jyung <huntthetux@gmail.com>
2025-07-08 11:38:23 +0800

    fix no temp

--- blocks.def.h
+++ blocks.def.h
@@ -2,7 +2,7 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"🎧", "~/get_volume.sh",	0,		10},
-	{"🌡️", "echo $(($(cat /sys/class/thermal/thermal_zone0/temp) / 1000))\"°C\"",	5,		0},
+  {"🌡️", "echo $(sensors -j k10temp-pci-* | jq '.[\"k10temp-pci-00c3\"].Tctl.temp1_input | round')°C", 5, 0},
 	{"", "top -bn1 | grep \"Cpu(s)\" | sed \"s/.*, *\\([0-9.]*\\)%* id.*/\\1/\" | awk '{printf \"🖥️%5.1f%%\", 100 - $1}'",	3,		0},
 	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	5,		0},
 	// {" ", "echo $(cat /sys/class/power_supply/BAT0/capacity)\"%\"",	5,		0},
